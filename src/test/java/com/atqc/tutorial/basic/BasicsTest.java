a62c4a374101bf36be8c920b5f20a3f49a855fd2
YuriiVern <yuriy.vernygorov@gmail.com>
2026-01-08 00:31:31 +0200

    Init commit - Welcome to atqc training

---
+++ src/test/java/com/atqc/tutorial/basic/BasicsTest.java
@@ -0,0 +1,11 @@
+package com.atqc.tutorial.basic;
+
+import org.testng.annotations.Test;
+
+public class BasicsTest {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+}
