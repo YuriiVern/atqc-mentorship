bb408d36f957b3798cfefab93ed31212ee00714f
YuriiVern <yuriy.vernygorov@gmail.com>
2026-01-13 22:43:18 +0200

    Implement part_1 tests for basics

---
+++ src/main/java/com/atqc/configuration/NotCompletedTaskException.java
@@ -0,0 +1,8 @@
+package com.atqc.configuration;
+
+public class NotCompletedTaskException extends RuntimeException{
+
+    public NotCompletedTaskException() {
+        super("To implement in method");
+    }
+}
