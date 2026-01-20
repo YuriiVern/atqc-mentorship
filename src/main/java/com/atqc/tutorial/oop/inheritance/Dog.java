875dccb4b9e1652f2033070df08dcf0161b475cf
Yurii-Vernyhorov_fortive <yurii.vernyhorov@fluke.com>
2026-01-21 00:05:44 +0200

    [OOP] Inheritance

---
+++ src/main/java/com/atqc/tutorial/oop/inheritance/Dog.java
@@ -0,0 +1,7 @@
+package com.atqc.tutorial.oop.inheritance;
+
+
+public class Dog extends  Animal implements Pet {
+
+
+}
---
+++ src/main/java/com/atqc/tutorial/oop/inheritance/Pet.java
@@ -0,0 +1,32 @@
+package com.atqc.tutorial.oop.inheritance;
+
+
+/**
+ *
+ * Pet interface requirements:
+ * - Methods:
+ *   - String getOwnerName()
+ *   - void setOwnerName(String ownerName)
+ *
+ * Dog class requirements (Part 2):
+ * - Dog must implement Pet interface.
+ * - Add field:
+ *   - String ownerName
+ *
+ * - Implement all methods from Pet interface.
+ *
+ * Theory question (Part 2):
+ * Explain the following code line:
+ *
+ * Pet pet = new Dog();
+ *
+ * Explain:
+ * - What is the type of variable pet?
+ * - What methods are available through this reference?
+ */
+
+
+public interface Pet {
+
+
+}
